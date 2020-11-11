f5bebc19995b0654ad3886a7847f16ae0a0531a6
Amar Lakshya <axx945@cs.bham.ac.uk>
2020-11-11 02:37:26 +0000

    ADDED: description

--- popup/replace_text.js
+++ popup/replace_text.js
@@ -1,11 +1,11 @@
 document.querySelector("#submit").addEventListener("click", (e) => {
-  let oldText = document.getElementById("oldText").value;
-  let newText = document.getElementById("newText").value;
-  browser.tabs.executeScript(null, {
-    file: "/content_scripts/replaycer.js"
-  }).then(() => { // use the Promise prevent "Error: Could not establish connection. Receiving end does not exist."
-    browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-      browser.tabs.sendMessage(tabs[0].id, {oldText, newText});
+    let oldText = document.getElementById("oldText").value;
+    let newText = document.getElementById("newText").value;
+    browser.tabs.executeScript(null, {
+        file: "/content_scripts/replaycer.js"
+    }).then(() => { // use the Promise prevent "Error: Could not establish connection. Receiving end does not exist."
+        browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id, {oldText, newText});
+        });
     });
-  });
 });
